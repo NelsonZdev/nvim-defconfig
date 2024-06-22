2519acef8e7a2926d2ec4aa582ca9738d1fa35f9
Nelson Zapata <stiven101998@gmail.com>
2024-06-22 11:45:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y
+
+# Snap Stable version
+sudo snap install --beta nvim --classic
+
 # Unix/Linux
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
